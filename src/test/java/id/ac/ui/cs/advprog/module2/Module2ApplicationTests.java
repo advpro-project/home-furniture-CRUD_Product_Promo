6aa073186730df7da88ac11c18a54cd98977788b
Devandra <devandra271203@gmail.com>
2024-04-08 12:44:01 +0700

    Initial Commit

---
+++ src/test/java/id/ac/ui/cs/advprog/module2/Module2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.module2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
