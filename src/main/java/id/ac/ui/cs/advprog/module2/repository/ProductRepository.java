180aad1edd4b70d0076ea8ff05d415dce37e181d
Devandra <devandra271203@gmail.com>
2024-04-24 20:19:10 +0700

    [ADD] added Product & PromoCode Repository.

---
+++ src/main/java/id/ac/ui/cs/advprog/module2/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.module2.repository;
+import id.ac.ui.cs.advprog.module2.model.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ProductRepository {
+    Product addProduct(Product product) {
+        return null;
+    }
+    Product updateProduct(UUID productId, Product product) {
+        return null;
+    }
+    void deleteProduct(UUID productId) {
+        
+    }
+    Product getProductById(UUID productId) {
+        return null;
+    }
+    List<Product> getTop10PopularProducts() {
+        return null;
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/module2/repository/PromoCodeRepository.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.module2.repository;
+import id.ac.ui.cs.advprog.module2.model.PromoCode;
+
+import java.util.UUID;
+
+public class PromoCodeRepository {
+    PromoCode addPromoCode(PromoCode promoCode) {
+        return null;
+    }
+    PromoCode updatePromoCode(UUID promoId, PromoCode promoCode) {
+        return null;
+    }
+    void deletePromoCode(UUID promoId) {
+        
+    }
+    PromoCode getPromoCodeById(UUID promoId) {
+        return null;
+    }
+}
