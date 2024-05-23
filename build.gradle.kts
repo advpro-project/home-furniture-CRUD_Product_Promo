c27e487eb968a7bbe6959c233546b85fe11aa6f4
NI04Code <naufalichsan114@gmail.com>
2024-05-23 17:45:49 +0700

    [DEPLOY] test actuator deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.17.1")
     implementation("org.apache.logging.log4j:log4j-api:2.17.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.withType<Test> {
